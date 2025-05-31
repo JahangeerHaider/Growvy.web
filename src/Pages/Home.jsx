1bc8bf89d23f3717b225219d01a78a99897fb50b
Jahangeer Haider <jahangeerhaider047@gmail.com>
2025-06-01 01:19:06 +0500

    Added gethired component with responsive layout and box styling

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import About from '../components/About'
 import TargetAudience from '../components/targetAudience'
+import GetHired from '../components/GetHired'
 
 
 function Home() {
@@ -10,6 +11,7 @@ function Home() {
       <Hero/>
       <About description=" “Growvy is built to streamline hiring, optimize assets, and drive business growth. Our mission is to create a tech-driven ecosystem where companies hire smarter, manage resources efficiently, and scale effortlessly.Whether you're seeking top talent, asset solutions, or business expansion, Growvy is your trusted partner in success.”"/>
       <TargetAudience/>
+      <GetHired/>
     </div>
   )
 }
---
+++ src/components/GetHired.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './GetHired.css'
+
+function GetHired() {
+  return (
+    <div className='contianer'>
+        <div className="hired-content">
+            <h1>Join Growvy’s 50M+ Job Seeker</h1>
+            <h1>Community & Get Hired Faster!</h1>
+            <p className='description'>“Build your profile, get noticed by top employers, and access exclusive job
+            opportunities tailored to your skills. Your next career move starts here!”</p>
+            <button>Sign in</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default GetHired
